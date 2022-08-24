077681413e7acb9d8ec591c7c466e94d6e18f110
Shanawaz-git <flamefayaz03@gmail.com>
2022-08-24 16:26:08 +0530

    SECOND

--- controllers/points.js
+++ controllers/points.js
@@ -4,7 +4,11 @@ exports.cctest = (req, res) => {
   var c1 = req.body.newControls;
   var tot = a1 + b1 + c1;
   var score = tot / 3;
-  res.json({ Score: score });
+  res
+    .status(200)
+    .send("Hello, Server is running good, have nice day. ->FROM GITHUB<-")
+    .end();
+  // res.json({ Score: score });
 };
 
 exports.ccget = (req, res) => {
